b1b0d3edbf424960f9ad93d1e4f22001c5808ccd
karulation <125230705+karulation@users.noreply.github.com>
2024-05-23 23:36:05 +0800

    Update main.js

--- plugins/menu/main.js
+++ plugins/menu/main.js
@@ -39,7 +39,7 @@ const defaultMenu = {
 let handler = async (m, { conn, usedPrefix: _p, __dirname, isPrems }) => {
 	try {
 		let meh = padLead(ranNumb(43), 3)
-		let nais = "https://telegra.ph/file/8936efcdafccc617f924c.png";
+		let nais = await (await fetch('https://raw.githubusercontent.com/arasea2/DB/main/honkai.json')).json().then(v => v.getRandom())
 		let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
 		let { limit, role } = db.data.users[m.sender]
 		let name = await conn.getName(m.sender).replaceAll('\n','')
