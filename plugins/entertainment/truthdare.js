a5d966788e8c5167ac80e6f89926bc377d545057
karulation <125230705+karulation@users.noreply.github.com>
2024-05-23 09:12:20 +0800

    Update truthdare.js

--- plugins/entertainment/truthdare.js
+++ plugins/entertainment/truthdare.js
@@ -1,15 +1,15 @@
-import { truth, dare } from '@bochilteam/scraper'
+// import { truth, dare } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-	let anu = await (/dare/g.test(command) ? dare : truth)()
-	m.reply(anu)
-}
+// let handler = async (m, { conn, usedPrefix, command }) => {
+// 	let anu = await (/dare/g.test(command) ? dare : truth)()
+// 	m.reply(anu)
+// }
 
-handler.help = ['truth','dare']
-handler.tags = ['entertainment']
-handler.command = /^(truth|dare)$/i
+// handler.help = ['truth','dare']
+// handler.tags = ['entertainment']
+// handler.command = /^(truth|dare)$/i
 
-handler.limit = true
-handler.group = true
+// handler.limit = true
+// handler.group = true
 
-export default handler
+// export default handler
