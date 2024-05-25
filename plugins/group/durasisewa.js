91e75c89a39e41403459e7ce7fe0dd753d9e16d8
karulation <125230705+karulation@users.noreply.github.com>
2024-05-25 14:36:09 +0800

    Update durasisewa.js

--- plugins/group/durasisewa.js
+++ plugins/group/durasisewa.js
@@ -1,15 +1,15 @@
-import db from '../../lib/database.js'
+// import db from '../../lib/database.js'
 
-let handler = async (m, { conn }) => {
-	let chat = db.data.chats[m.chat]
-	if (chat.expired == 0 || chat.expired == null) return m.reply(`[ ! ] Bot join permanen, tidak ada durasi.`)
-	await conn.reply(m.chat, `Sisa Waktu Sewa :\n\n*${((chat.joindate + chat.joincd) - new Date()).toTimeString()}*`, m)
-}
+// let handler = async (m, { conn }) => {
+// 	let chat = db.data.chats[m.chat]
+// 	if (chat.expired == 0 || chat.expired == null) return m.reply(`[ ! ] Bot join permanen, tidak ada durasi.`)
+// 	await conn.reply(m.chat, `Sisa Waktu Sewa :\n\n*${((chat.joindate + chat.joincd) - new Date()).toTimeString()}*`, m)
+// }
 
-handler.menugroup = ['durasisewa']
-handler.tagsgroup = ['group']
-handler.command = /^((cek)?(durasi|info)(sewa|join)(bot)?)$/i
+// handler.menugroup = ['durasisewa']
+// handler.tagsgroup = ['group']
+// handler.command = /^((cek)?(durasi|info)(sewa|join)(bot)?)$/i
 
-handler.group = true
+// handler.group = true
 
-export default handler
+// export default handler
