b5b6fd0b7e8f19408e55878fd2533400a10bac80
karulation <125230705+karulation@users.noreply.github.com>
2024-05-25 14:33:20 +0800

    Update lolice.js

--- plugins/entertainment/lolice.js
+++ plugins/entertainment/lolice.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedprefix }) => {
 	let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	await conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/lolice', {
 		avatar: await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-	}), '', `liuliuliuliuliu kami dengar disini ada lolicon`, m)
+	}), '', `liuliuliuliuliu Nao hear that there's Lolicon here?!`, m)
 }
 
 handler.help = ['lolice']
