5464b5405bcbcf7bcb832ab7eb6867772b63a8c8
karulation <125230705+karulation@users.noreply.github.com>
2024-05-29 11:10:47 +0800

    Update ceknsfw.js

--- plugins/info/ceknsfw.js
+++ plugins/info/ceknsfw.js
@@ -1,31 +1,31 @@
-import uploadImage from '../../lib/uploadImage.js'
+// import uploadImage from '../../lib/uploadImage.js'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = (q.msg || q).mimetype || q.mediaType || ''
-	if ((/image/g.test(mime) && !/webp/g.test(mime))) {
-		try {
-			let img = await q.download?.()
-			let out = await uploadImage(img)
-			let res = await fetch(`https://api.lolhuman.xyz/api/nsfwcheck?apikey=${api.lol}&img=${out}`)
-			let json = await res.json()
-			if (json.status != '200') throw `Feature Error!`
-			let get_result = json.result
-			let is_nsfw = 'No'
-			if (Number(get_result.replace("%", "")) >= 50) is_nsfw = 'Yes'
-			m.reply(`Is NSFW? *${is_nsfw}*\n\nNSFW Score : *${get_result}*`)
-		} catch (e) {
-			console.log(e)
-			m.reply('Fitur Error!')
-		}
-	} else m.reply(`Send picture with caption *${usedPrefix + command}* or tag a picture`)
-}
+// let handler = async (m, { conn, args, usedPrefix, command }) => {
+// 	let q = m.quoted ? m.quoted : m
+// 	let mime = (q.msg || q).mimetype || q.mediaType || ''
+// 	if ((/image/g.test(mime) && !/webp/g.test(mime))) {
+// 		try {
+// 			let img = await q.download?.()
+// 			let out = await uploadImage(img)
+// 			let res = await fetch(`https://api.lolhuman.xyz/api/nsfwcheck?apikey=${api.lol}&img=${out}`)
+// 			let json = await res.json()
+// 			if (json.status != '200') throw `Feature Error!`
+// 			let get_result = json.result
+// 			let is_nsfw = 'No'
+// 			if (Number(get_result.replace("%", "")) >= 50) is_nsfw = 'Yes'
+// 			m.reply(`Is NSFW? *${is_nsfw}*\n\nNSFW Score : *${get_result}*`)
+// 		} catch (e) {
+// 			console.log(e)
+// 			m.reply('Fitur Error!')
+// 		}
+// 	} else m.reply(`Send picture with caption *${usedPrefix + command}* or tag a picture`)
+// }
 
-handler.help = ['checknsfw']
-handler.tags = ['information']
-handler.command = /^(ch?ec?k)nsfw|nsfw(ch?ec?k)$/i
+// handler.help = ['checknsfw']
+// handler.tags = ['information']
+// handler.command = /^(ch?ec?k)nsfw|nsfw(ch?ec?k)$/i
 
-handler.premium = true
-handler.limit = true
+// handler.premium = true
+// handler.limit = true
 
-export default handler
+// export default handler
