9113f3a4ddd4406c2b48fd964114bbd2bc3ddee5
karulation <125230705+karulation@users.noreply.github.com>
2024-05-29 20:21:02 +0800

    Update getID.js

--- plugins/owner/getID.js
+++ plugins/owner/getID.js
@@ -1,19 +1,24 @@
-import fs from 'fs'
+import fs from 'fs';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	m.reply(`_[!] Please wait master, I'm sending the chat ID of this group to you privately. . ._`)
-	try {
-		await conn.sendMessage(m.sender, `Chat ID: ${m.chat}`, { quoted: m })
-	} catch (e) {
-		console.log(e)
-		m.reply('Error occured, check logs.')
-	}
-}
+  // Send a reply message to the group indicating that the process has started
+  m.reply(`_[!] Please wait master, I'm sending the chat ID of this group to you privately..._`);
 
-handler.menuowner = ['getid']
-handler.tagsowner = ['owner']
-handler.command = /^(getid)$/i
+  try {
+    // Send the chat ID to the user's private chat
+    await conn.sendMessage(m.sender, { text: `Chat ID: ${m.chat}` }, { quoted: m });
+  } catch (e) {
+    // Log the error for debugging purposes
+    console.error('Error occurred while sending the message:', e);
+    // Send an error message to the group
+    m.reply('An error occurred. Please check the logs.');
+  }
+};
 
-handler.owner = true
+// Define handler properties
+handler.menuowner = ['getid'];
+handler.tagsowner = ['owner'];
+handler.command = /^(getid)$/i;
+handler.owner = true;
 
-export default handler
+export default handler;
