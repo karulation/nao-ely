5909a507eae8f291a8fb9020b65ed37b9ac4ef8a
karulation <125230705+karulation@users.noreply.github.com>
2024-05-29 13:55:07 +0800

    Update getID.js

--- plugins/owner/getID.js
+++ plugins/owner/getID.js
@@ -1,10 +1,13 @@
-import db from '../../lib/database.js'
+import fs from 'fs'
 
-let handler = async (m, { args }) => {
-	let id = args[0] ? args[0] : m.isGroup ? m.chat : ''
-	if (!id) throw `input group id !`
-	if (!chat) return m.reply(`[!] Invalid ID Group`)
-	await m.reply('Yaho Karu! Here is the ID : '+m.chat);
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	m.reply(`_[!] Please wait master, I'm sending the chat ID of this group to you privately. . ._`)
+	try {
+		await conn.sendMessage(m.sender, `Chat ID: ${m.chat}`, { quoted: m })
+	} catch (e) {
+		console.log(e)
+		m.reply('Error occured, check logs.')
+	}
 }
 
 handler.menuowner = ['getid']
