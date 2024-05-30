3a69d878867b812eaab4b2eaa1402e5befe58c85
karulation <125230705+karulation@users.noreply.github.com>
2024-05-30 14:25:46 +0800

    Update pengumuman.js

--- plugins/group/pengumuman.js
+++ plugins/group/pengumuman.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || q.mediaType || ''
 	text = text ? text : m.quoted?.text ? m.quoted.text : m.quoted?.caption ? m.quoted.caption : m.quoted?.description ? m.quoted.description : ''
-	if (!text) throw `Example : ${usedPrefix + command} ayo mabar`
+	if (!text) throw `Example : ${usedPrefix + command} Nao-chan wa kyou mo kawai~~~`
 	if (/video|image/g.test(mime) && !/webp/g.test(mime)) {
 		let media = await q.download?.()
 		await conn.sendFile(m.chat, media, '', text, null, false, { mentions: participants.map(a => a.id), quoted: fkontak })
@@ -14,7 +14,6 @@ handler.tagsgroup = ['group']
 handler.command = /^(pengumuman|announce|hidd?en?tag)$/i
 
 handler.admin = true
-handler.botAdmin = true
 handler.group = true
 
 export default handler
