23c0b0de436f8781761e49d4dc9d2eb07b3df4b1
karulation <125230705+karulation@users.noreply.github.com>
2024-05-30 10:50:16 +0800

    Create zen.js

---
+++ plugins/neo/zen.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+    try {
+        var media = 'https://telegra.ph/file/81acc75d9671ba3f8401a.jpg';
+        var textMessage = "*NEO STAFF INFO*\nName : Zen\nPosition : Second Tier Neo Anti - Skill\nJoined Group: Neo Asterisk, Neo Arcus, NeoAnicom Lounge\nBirthday: 5/5\nInstagram: yasashiisuisei\nTiktok: yasashijanai\nDescription: Zen or Zenitsu is a man that does exist from an anime yet he is definitely the most coward and hates himself more than others... but other than that he does have hidden strength that he is not discovered yet...\n\nYoroshikune Onegaishimasu Minna 💛\n=======================";
+        
+        await conn.sendFile(m.chat, media, '', textMessage, m);
+    } catch (e) {
+        m.reply('An error occurred, please contact Karu.');
+        console.error(e);
+    }
+};
+
+handler.help = ['zen'];
+handler.tags = ['Neo Intro'];
+handler.command = /^(zen)$/i;
+
+export default handler;
