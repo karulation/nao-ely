495f6f9bb91adc394ae31ce6f11906f347b714ea
karulation <125230705+karulation@users.noreply.github.com>
2024-05-30 09:41:20 +0800

    Update lolice.js

--- plugins/entertainment/lolice.js
+++ plugins/entertainment/lolice.js
@@ -1,15 +1,15 @@
-let handler = async (m, { conn, usedprefix }) => {
-	let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-	await conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/jail', {
-		avatar: await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-	}), '', `liuliuliuliuliu Nao hear that there's Lolicon here?!`, m)
-}
+// let handler = async (m, { conn, usedprefix }) => {
+// 	let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+// 	await conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/jail', {
+// 		avatar: await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+// 	}), '', `liuliuliuliuliu Nao hear that there's Lolicon here?!`, m)
+// }
 
-handler.help = ['lolice']
-handler.tags = ['entertainment']
-handler.command = /^(lolice)$/i
+// handler.help = ['lolice']
+// handler.tags = ['entertainment']
+// handler.command = /^(lolice)$/i
 
-handler.premium = true
-handler.limit = true
+// handler.premium = true
+// handler.limit = true
 
-export default handler
+// export default handler
