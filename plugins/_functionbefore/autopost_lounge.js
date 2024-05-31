e3a95642470ed3b329fd52a02a7acf84568f6468
karulation <125230705+karulation@users.noreply.github.com>
2024-05-31 09:12:00 +0800

    Update autopost_lounge.js

--- plugins/_functionbefore/autopost_lounge.js
+++ plugins/_functionbefore/autopost_lounge.js
@@ -3,7 +3,7 @@ import db from '../../lib/database.js';
 export async function before(m, { conn, text, participants }) {
     // Group IDs
     const groupID = '120363020837863962@g.us'; // Replace with actual group ID
-    const gamingGroup = '120363226270078711@g.us'; // Replace with actual gaming group ID
+    const gamingGroups = ['120363226270078711@g.us']; // Replace with actual gaming group ID
     const nonGamingGroups = ['60177637943-1627735681@g.us', '60177637943-1634746023@g.us']; // Replace with actual non-gaming group IDs
 
     // Function to check if a string only contains emojis or is empty
@@ -32,11 +32,14 @@ export async function before(m, { conn, text, participants }) {
         if (isGamingMessage(m.text)) {
             if (/video|image/g.test(mime) && !/webp/g.test(mime)) {
                 // Gaming message with media
-                let media = await q.download?.();
-                await conn.sendFile(gamingGroup, media, '', textMessage, null, false, { mentions });
+                for (const gamingGroup of gamingGroups) {
+                    let media = await q.download?.();
+                    await conn.sendFile(gamingGroup, media, '', textMessage, null, false, { mentions });
+                }
             } else {
-                // Gaming text message
-                await conn.reply(gamingGroup, textMessage, null, { mentions });
+                for (const gamingGroup of gamingGroups) {
+                    await conn.reply(gamingGroup, textMessage, null, { mentions });
+                }
             }
         } else {
             if (/video|image/g.test(mime) && !/webp/g.test(mime)) {
@@ -62,7 +65,7 @@ function isGamingMessage(text) {
     const gamingKeywords = [
         'wild rift', 'mobile legends', 'league of legends', 'dota', 'pubg', 'fortnite', 
         'apex legends', 'valorant', 'honkai', 'wuthering waves', 'blue archive', 
-        'star rail', 'genshin', 'call of duty'
+        'star rail', 'genshin', 'call of duty', 'epic games', 'riot games', 'mihoyo', 'esports'
     ];
     const lowerText = text.toLowerCase(); // Convert text to lowercase for case-insensitive comparison
     return gamingKeywords.some(keyword => lowerText.includes(keyword)); // Check if any keyword exists in the text
