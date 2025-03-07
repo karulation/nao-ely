b20862931017f601662e32e53a5c8f7551f5fbd4
Karu <shafiqhaikal1763@gmail.com>
2025-03-08 05:27:29 +0800

    changed owner name

--- plugins/group/creator.js
+++ plugins/group/creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
     let ownerNumber = "60177637943";
-    let ownerName = "Owner"; // Change this if you want a specific name
+    let ownerName = "Karu Shion (Nao Developer)"; // Change this if you want a specific name
 
     const vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${ownerName};;;\nFN:${ownerName}\nTEL;type=Mobile;waid=${ownerNumber}:${ownerNumber}\nEND:VCARD`;
 
