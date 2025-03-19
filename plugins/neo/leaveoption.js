c59ad4365a9bef5106ead8caf0653f406e6f1bda
Karu <shafiqhaikal1763@gmail.com>
2025-03-19 12:58:39 +0800

    update leaveoption

--- plugins/neo/leaveoption.js
+++ plugins/neo/leaveoption.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix, command, text }) => {
     try {
         var media = 'https://telegra.ph/file/aa321d2670c88d1bc835d.jpg';
-        var textMessage = '🌟 *Leaving is Always an Option* 🌟\n\nIf you find yourself disagreeing with our rules or administrative approach, the **“Leave Group”** button is readily available.\n\n🔹 *Every community is unique.*  Please respect that we operate with our own set of guidelines and management style. Refrain from comparing us to other groups — especially those with a more “anarchy” style of management.\n\n✨ While we may share the same genre or interests, **Neo** is built on structure, harmony, and respect — values that define who we are.\n\nLet’s appreciate the diversity between communities and focus on making this one a better place for all. 💫';
+        var textMessage = '🌟 *Leaving is Always an Option* 🌟\n\nIf you find yourself disagreeing with our rules or administrative approach, the “*Leave Group*” button is readily available.\n\n🔹 *Every community is unique.*  Please respect that we operate with our own set of guidelines and management style. Refrain from comparing us to other groups — especially those with a more “anarchy” style of management.\n\n✨ While we may share the same genre or interests, *Neo* is built on structure, harmony, and respect — values that define who we are.\n\nLet’s appreciate the diversity between communities and focus on making this one a better place for all. 💫';
         
         await conn.sendFile(m.chat, media, '', textMessage, m);
     } catch (e) {
