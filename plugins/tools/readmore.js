a4f4c5c8da1616998b2980f6a07fd3eaaeebe479
Karu <shafiqhaikal1763@gmail.com>
2025-03-28 13:04:48 +0800

    comment tools file

--- plugins/tools/readmore.js
+++ plugins/tools/readmore.js
@@ -1,15 +1,15 @@
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-	if (!text) throw `Usage :\n*${usedPrefix+command} text1 | text2*`
-	let [l, r] = text.split`|`
-	if (!r) r = ''
-	m.reply(l + readMore + r)
-}
+// let handler = async (m, { conn, usedPrefix, command, text }) => {
+// 	if (!text) throw `Usage :\n*${usedPrefix+command} text1 | text2*`
+// 	let [l, r] = text.split`|`
+// 	if (!r) r = ''
+// 	m.reply(l + readMore + r)
+// }
 
-handler.help = ['readmore <teks>|<teks>']
-handler.tags = ['tools']
-handler.command = /^(spoiler|hidetext|(read)?more|selengkapnya)$/i
+// handler.help = ['readmore <teks>|<teks>']
+// handler.tags = ['tools']
+// handler.command = /^(spoiler|hidetext|(read)?more|selengkapnya)$/i
 
-export default handler
+// export default handler
 
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+// const more = String.fromCharCode(8206)
+// const readMore = more.repeat(4001)
